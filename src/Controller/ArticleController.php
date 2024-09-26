9527c00c90f583d18f97623cb1a866582be1be4d
yega <mdolma@ymail.com>
2024-09-26 12:32:17 +0200

    pagination to the articles integrated

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -3,18 +3,25 @@
 namespace App\Controller;
 
 use App\Repository\ArticleRepository;
+use Knp\Component\Pager\PaginatorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 class ArticleController extends AbstractController
 {
     #[Route('/article', name: 'app_article')]
-    public function all(ArticleRepository $ar): Response
+    public function all(ArticleRepository $ar, PaginatorInterface $paginator, Request $request): Response
     {
         $articles = $ar->findAll();
+        $pagination = $paginator->paginate(
+            $articles, /* query NOT result in the form of an array or persistant collection and collection array  */
+            $request->query->getInt('page', 1), /*page number, looks for a page variable inside the url*/
+            10 /*limit per page*/
+        );
         return $this->render('article/all.html.twig', [
-            'articles' => $articles,
+            'pagination' => $pagination,
         ]);
     }
 }
